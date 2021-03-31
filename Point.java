285e3d3c37fb95b7011bf6c70b81ed619c6345c8
LucaMagnin <79831128+LucaMagnin@users.noreply.github.com>
2021-03-31 16:40:51 +0200

    code

---
+++ Point.java
@@ -0,0 +1,8 @@
+public class Point{
+	int x;
+	int y;
+	public Point(int abscisse, int ordonnée){
+		x=abscisse;
+		y=ordonnée;
+	}
+}
---
+++ Tetraminos.java
@@ -0,0 +1,26 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Tetraminos {
+	
+	
+	public Point[] coordsTetrimino;
+	public Color couleurTetra;
+	
+	public Tetraminos(Point[] coords,Color couleur){
+		coordsTetrimino=coords;
+		couleurTetra=couleur;
+
+
+	}
+	/*public int[][] TetraminoAleatoire(){
+		int a=(int)(Math.random()*7);
+		int[][] TetraminoAleatoire = coordsTetrimino[a];
+		return TetraminoAleatoire;
+	}*/
+	
+}
+		
+		
+	
+
