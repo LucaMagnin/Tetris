1dadae32d7546b99287e0ec218ca0580ce6dbc69
LucaMagnin <79831128+LucaMagnin@users.noreply.github.com>
2021-04-01 15:44:52 +0200

    Update ControleGrille.java

--- ControleGrille.java
+++ ControleGrille.java
@@ -66,7 +66,7 @@ public class ControleGrille  {
         return t;
 	}
 	
-		public boolean ObjetDroite(){
+	public boolean ObjetDroite(){
 		int i=0;
 		boolean t = false;
 		while(!t && i<tetraminoActuel.coordsTetrimino.length){
@@ -82,7 +82,7 @@ public class ControleGrille  {
 		}
         return t;
 	}
-		public boolean ObjetGauche(){
+	public boolean ObjetGauche(){
 		int i=0;
 		boolean t = false;
 		while(!t && i<tetraminoActuel.coordsTetrimino.length){
