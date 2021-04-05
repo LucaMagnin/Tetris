440b4d97907b20ec74f46e92f25201e800a59eca
LucaMagnin <79831128+LucaMagnin@users.noreply.github.com>
2021-04-05 20:33:50 +0200

    Update Tetraminos.java

--- Tetraminos.java
+++ Tetraminos.java
@@ -6,12 +6,17 @@ public class Tetraminos {
 	
 	public Point[] coordsTetrimino;
 	public Color couleurTetra;
-	
+	public int[][] origineTetramino;
+	public Tetraminos(Point[] coords,Color couleur,int[][] origine){
+		coordsTetrimino=coords;
+		couleurTetra=couleur;
+		origineTetramino=origine;
+
+
+	}
 	public Tetraminos(Point[] coords,Color couleur){
 		coordsTetrimino=coords;
 		couleurTetra=couleur;
-
-
 	}
 	/*public int[][] TetraminoAleatoire(){
 		int a=(int)(Math.random()*7);
