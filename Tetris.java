650fafdab7d18f37a97fab2786c49735987a947d
LucaMagnin <79831128+LucaMagnin@users.noreply.github.com>
2021-04-08 10:35:12 +0200

    Tetris=Test

--- Tetris.java
+++ Tetris.java
@@ -1,10 +1,6 @@
-
-public class Tetris {
-	
-	public static void main (String[] args) {
-		
+public class Tetris{
+	public static void main (String[] args)  {
+		new Interfacedepart ();
 	}
 }
 
-//Bonsoir, je suis la main
-//coucou
