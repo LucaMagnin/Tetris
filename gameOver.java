4b29427f7fe61aa573d936d229ccff781b4258ff
julieste <moi.julie.ster@gmail.com>
2021-04-11 19:19:26 +0200

    Create gameOver.java

---
+++ gameOver.java
@@ -0,0 +1,31 @@
+import javax.swing.*;
+import java.awt.*;
+public class gameOver extends JFrame{
+	
+	public gameOver(){
+		setTitle("Game Over"); 
+        setSize(500,500);	
+        setLocation(100,200);
+        
+        JLabel perdu = new JLabel("GAME OVER");
+        perdu.setBounds(180,70,300,100);
+        perdu.setForeground(Color.blue);
+        //panneau fond
+		JLabel Fond = new JLabel(new ImageIcon("./Rf838b0264611378bc84338f70571ed1c.png"));
+		Fond.setBounds(0,0,this.getWidth(),this.getHeight());
+        
+        perdu.setFont(new java.awt.Font(Font.SERIF,Font.BOLD,40));
+        
+        JPanel panneauFinal = new JPanel(); 
+		panneauFinal.setLayout(null);
+		panneauFinal.add(perdu); 
+		panneauFinal.setBounds(100,200,500,500);
+		
+		panneauFinal.add(Fond);
+		add(panneauFinal);
+		setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
+}
+
