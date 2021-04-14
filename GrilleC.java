c0f99d95d984ed878cb5a7f14a6f1221e46d50f2
LucaMagnin <79831128+LucaMagnin@users.noreply.github.com>
2021-04-14 18:31:19 +0200

    Début de tetris

--- GrilleC.java
+++ GrilleC.java
@@ -24,10 +24,12 @@ public class GrilleC extends JPanel { //la grille est un panneau
 		super();
 		JPanel PanneaumaGrille = this;
 		monControleGrille.initialiserGrille();
+		initialiserTimer();
+
 
 	}
  
-	public void paint(Graphics g){ 
+	public void paintComponent(Graphics g){ 
 		int i=0;
 
 		for(int x =100; x <= 420; x += 28 ){
@@ -41,8 +43,18 @@ public class GrilleC extends JPanel { //la grille est un panneau
 			}
 			i++;
 		}
-
+		DessinerPiece(g);
 	} 
+	
+	public void DessinerPiece(Graphics g){
+		for(int i=0;i<4;i++){
+			g.setColor(monControleGrille.tetraminoActuel.couleurTetra);
+			int origineX=100+(monControleGrille.tetraminoActuel.origineTetramino.x+monControleGrille.tetraminoActuel.coordsTetrimino[i].x)*28+1;
+			int origineY=100+(monControleGrille.tetraminoActuel.origineTetramino.y+monControleGrille.tetraminoActuel.coordsTetrimino[i].y)*28+1;
+			g.fillRect(origineX,origineY,26,26);
+		}
+	}
+	
 	public void initialiserTimer(){
 		Timer chrono = new Timer();
 		chrono.schedule(new TimerTask(){
@@ -52,5 +64,7 @@ public class GrilleC extends JPanel { //la grille est un panneau
 			}
 		},1000,1000);
 	}
+	
+		
 }
  
