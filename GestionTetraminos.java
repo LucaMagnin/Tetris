925971c4492e8194623aebecf881b48be32c3fd8
camillesbr <79834127+camillesbr@users.noreply.github.com>
2021-04-26 08:14:15 +0200

    Update GestionTetraminos.java

--- GestionTetraminos.java
+++ GestionTetraminos.java
@@ -1,1 +1,29 @@
-import java.util.*;import java.awt.Color;import java.awt.Graphics;public class GestionTetraminos {	Tetraminos[] tabTetraminos;	public GestionTetraminos(){		Tetraminos Z = new Tetraminos(new Point[]{new Point(0, -1), new Point(0, 0), new Point(-1, 0), new Point(-1, 1)},Color.red);		Tetraminos S = new Tetraminos(new Point[]{new Point(0, -1), new Point(0, 0), new Point(1, 0), new Point(1, 1)},Color.green);		Tetraminos I = new Tetraminos(new Point[]{new Point(0, -1), new Point(0, 0), new Point(0, 1), new Point(0, 2)},Color.cyan);		Tetraminos T = new Tetraminos(new Point[]{new Point(-1, 0), new Point(0, 0), new Point(1, 0), new Point(0, 1)},Color.magenta);		Tetraminos O = new Tetraminos(new Point[]{new Point(0, 0), new Point(1, 0), new Point(0, 1), new Point(1, 1)},Color.yellow);		Tetraminos L = new Tetraminos(new Point[]{new Point(-1, -1), new Point(0, -1), new Point(0, 0), new Point(0, 1)},Color.orange);		Tetraminos J = new Tetraminos(new Point[]{new Point(1, -1), new Point(0, -1), new Point(0, 0), new Point(0, 1)},(new Color(16,5,108)));				tabTetraminos = new Tetraminos[] {Z,S,T,O,L,J,I};		}	public Tetraminos TetraminoAleatoire(){		return tabTetraminos[(int)(Math.random()*7)];	}}			
+import java.util.*;
+import java.awt.Color;
+import java.awt.Graphics;
+public class GestionTetraminos {
+	Tetraminos[] tabTetraminos;
+	public GestionTetraminos(){
+
+
+		Tetraminos Z = new Tetraminos(new Point[]{new Point(0, -1), new Point(0, 0), new Point(-1, 0), new Point(-1, 1)},Color.red);
+		Tetraminos S = new Tetraminos(new Point[]{new Point(0, -1), new Point(0, 0), new Point(1, 0), new Point(1, 1)},Color.green);
+		Tetraminos I = new Tetraminos(new Point[]{new Point(0, -1), new Point(0, 0), new Point(0, 1), new Point(0, 2)},Color.cyan);
+		Tetraminos T = new Tetraminos(new Point[]{new Point(-1, 0), new Point(0, 0), new Point(1, 0), new Point(0, 1)},Color.magenta);
+		Tetraminos O = new Tetraminos(new Point[]{new Point(0, 0), new Point(1, 0), new Point(0, 1), new Point(1, 1)},Color.yellow);
+		Tetraminos L = new Tetraminos(new Point[]{new Point(-1, -1), new Point(0, -1), new Point(0, 0), new Point(0, 1)},Color.orange);
+		Tetraminos J = new Tetraminos(new Point[]{new Point(1, -1), new Point(0, -1), new Point(0, 0), new Point(0, 1)},Color.blue);
+		
+		tabTetraminos = new Tetraminos[] {Z,S,T,O,L,J,I};	
+	}
+	public Tetraminos TetraminoAleatoire(){
+		return tabTetraminos[(int)(Math.random()*7)];
+	}
+}
+
+		
+
+
+
+
+	
