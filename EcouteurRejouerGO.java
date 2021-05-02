d6bc1a7c48e82c0fe3cb1e61eb383b2419e54cfd
julieste <moi.julie.ster@gmail.com>
2021-05-02 11:03:56 +0200

    Update EcouteurRejouerGO.java

--- EcouteurRejouerGO.java
+++ EcouteurRejouerGO.java
@@ -1,15 +1,1 @@
-import java.awt.event.*;
-
-public class EcouteurRejouerGO implements ActionListener{
-		private gameOver go;
-	
-		public EcouteurRejouerGO(gameOver go){
-			this.go=go;
-		}
-	
-		public void actionPerformed(ActionEvent e)  {
-			go.fermeFen();
-			FenetreTetris f =new FenetreTetris();
-		}
-		
-	}	
+import java.awt.event.*;public class EcouteurRejouerGO implements ActionListener {	private gameOver go;		public EcouteurRejouerGO(gameOver go) {		this.go=go;	}		public void actionPerformed(ActionEvent e) {		go.fermeFen();		FenetreTetris f =new FenetreTetris();	}		}	
