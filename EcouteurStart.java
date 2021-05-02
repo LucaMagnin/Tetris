19a9f83bb02c6c523b8ff2484e3083429f132d9c
julieste <moi.julie.ster@gmail.com>
2021-05-02 11:03:54 +0200

    Update EcouteurStart.java

--- EcouteurStart.java
+++ EcouteurStart.java
@@ -1,14 +1,1 @@
-import java.awt.event.*;
-public class EcouteurStart implements ActionListener{
-		private Interfacedepart dep;
-	
-		public EcouteurStart(Interfacedepart dep){
-			this.dep=dep;
-		}
-	
-		public void actionPerformed(ActionEvent e)  {
-			FenetreTetris f =new FenetreTetris();
-			dep.fermeFen();
- 
-		} 
-	}
+import java.awt.event.*;public class EcouteurStart implements ActionListener {	private Interfacedepart dep;		public EcouteurStart(Interfacedepart dep) {		this.dep=dep;	}		public void actionPerformed(ActionEvent e) {		FenetreTetris f =new FenetreTetris();		dep.fermeFen();	} }
