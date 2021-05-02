14d3c11d347c473c65a0c3a08bd7476897c9b22c
julieste <moi.julie.ster@gmail.com>
2021-05-02 11:03:59 +0200

    Update EcouteurQuitterGO.java

--- EcouteurQuitterGO.java
+++ EcouteurQuitterGO.java
@@ -1,15 +1,1 @@
-import java.awt.event.*;
-	public class EcouteurQuitterGO implements ActionListener{
-		private gameOver go;
-	
-		public EcouteurQuitterGO(gameOver go){
-			this.go=go;
-		}
-	
-		public void actionPerformed(ActionEvent e)  {
-			go.fermeFen();
-			go.fenetreTetris.dispose();
-			
- 
-		} 
-	}
+import java.awt.event.*;public class EcouteurQuitterGO implements ActionListener {		private gameOver go;		public EcouteurQuitterGO(gameOver go) {		this.go=go;	}		public void actionPerformed(ActionEvent e) {		go.fermeFen();		go.fenetreTetris.dispose();	} }
