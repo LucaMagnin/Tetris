21efa69545f23efb9d64ef2058be53bf0834d886
julieste <moi.julie.ster@gmail.com>
2021-05-02 11:04:08 +0200

    Update EcouteurFermerRegle.java

--- EcouteurFermerRegle.java
+++ EcouteurFermerRegle.java
@@ -1,13 +1,1 @@
-import java.awt.event.*;
-public class EcouteurFermerRegle implements ActionListener{
-		private Regle reg;
-	
-		public EcouteurFermerRegle(Regle reg){
-			this.reg=reg;
-		}
-	
-		public void actionPerformed(ActionEvent e)  {
-			reg.fermeFen();
- 
-		} 
-	}
+import java.awt.event.*;public class EcouteurFermerRegle implements ActionListener {	private Regle reg;		public EcouteurFermerRegle(Regle reg) {		this.reg=reg;	}		public void actionPerformed(ActionEvent e) {		reg.fermeFen();	} }
