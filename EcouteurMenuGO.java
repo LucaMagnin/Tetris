fa4eb5748bc65bca40d042ecddb0f7fa6f6424d4
julieste <moi.julie.ster@gmail.com>
2021-05-02 11:04:05 +0200

    Update EcouteurMenuGO.java

--- EcouteurMenuGO.java
+++ EcouteurMenuGO.java
@@ -1,14 +1,1 @@
-import java.awt.event.*;
-	public class EcouteurMenuGO implements ActionListener{
-		private gameOver go;
-	
-		public EcouteurMenuGO(gameOver go){
-			this.go=go;
-		}
-	
-		public void actionPerformed(ActionEvent e)  {
-			new Interfacedepart();
-			go.fermeFen();
- 
-		} 
-	}
+import java.awt.event.*;public class EcouteurMenuGO implements ActionListener {		private gameOver go;		public EcouteurMenuGO(gameOver go) {		this.go=go;	}		public void actionPerformed(ActionEvent e) {		new Interfacedepart();		go.fermeFen();	} }
