900271c3bd15d1c8d99d2fd05f7c94118513d9e2
julieste <moi.julie.ster@gmail.com>
2021-05-02 11:03:37 +0200

    Update Tetraminos.java

--- Tetraminos.java
+++ Tetraminos.java
@@ -1,27 +1,1 @@
-import java.awt.Color;
-import java.awt.Graphics;
-
-public class Tetraminos {
-	
-	
-	public Point[] coordsTetrimino;
-	public Color couleurTetra;
-	public Point origineTetramino;
-	public Tetraminos(Point[] coords,Color couleur,Point origine){
-		coordsTetrimino=coords;
-		couleurTetra=couleur;
-		origineTetramino=origine;
-
-
-	}
-	public Tetraminos(Point[] coords,Color couleur){
-		coordsTetrimino=coords;
-		couleurTetra=couleur;
-	}
-
-	
-}
-		
-		
-	
-
+import java.awt.*;public class Tetraminos {	public Point[] coordsTetrimino;	public Color couleurTetra;	public Point origineTetramino;		public Tetraminos(Point[] coords,Color couleur,Point origine) {		coordsTetrimino=coords;		couleurTetra=couleur;		origineTetramino=origine;	}		public Tetraminos(Point[] coords,Color couleur) {		coordsTetrimino=coords;		couleurTetra=couleur;	}	}					
